0abc02d42ba882a0c36376e885329346b3ee8b64
oihoihoih <ochyuo@hotmail.com>
2024-09-11 07:33:48 +0200

    Check token endpoint added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,6 +12,8 @@ import {
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginDto, RegisterUserDto, UpdateAuthDto } from './dto';
 import { AuthGuard } from './guards/auth.guard';
+import { LoginResponse } from './interfaces/login-response';
+import { User } from './entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -20,14 +22,6 @@ export class AuthController {
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.authService.create(createUserDto);
-  }
-
-  @UseGuards(AuthGuard)
-  @Get()
-  findAll(@Request() req: Request) {
-    const user = req['user'];
-    return user;
-    // return this.authService.findAll();
   }
 
   @Get(':id')
@@ -53,5 +47,25 @@ export class AuthController {
   @Post('/register')
   register(@Body() registerDto: RegisterUserDto) {
     return this.authService.register(registerDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get()
+  findAll(@Request() req: Request) {
+    // const user = req['user'];
+    // return user;
+    return this.authService.findAll();
+  }
+
+  // LoginResponse
+  @UseGuards(AuthGuard)
+  @Get('check-token')
+  checkToken(@Request() req: Request): LoginResponse {
+    const user = req['user'] as User;
+
+    return {
+      user,
+      token: this.authService.getJwtToken({ id: user._id }),
+    };
   }
 }
