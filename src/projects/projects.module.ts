6306dc02b26bfec746f7728079e2c3cb131fee98
oihoihoih <ochyuo@hotmail.com>
2024-09-20 18:49:13 +0200

    Project schema added to db

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { ProjectsController } from './projects.controller';
+import { Project, ProjectSchema } from './entities/project.entity';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   controllers: [ProjectsController],
   providers: [ProjectsService],
+  imports: [
+    // 👇 import the MongooseModule
+    MongooseModule.forFeature([
+      {
+        name: Project.name,
+        schema: ProjectSchema,
+      },
+    ]),
+  ],
 })
 export class ProjectsModule {}
