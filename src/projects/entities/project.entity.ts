1e8bbb4ff6893e7dd0cea43ee2be71415f63ddc2
oihoihoih <ochyuo@hotmail.com>
2024-09-20 18:42:57 +0200

    Project schema created

--- src/projects/entities/project.entity.ts
+++ src/projects/entities/project.entity.ts
@@ -1,1 +1,25 @@
-export class Project {}
+import { Prop, SchemaFactory, Schema } from '@nestjs/mongoose';
+
+@Schema()
+export class Project {
+  _id?: string;
+
+  @Prop({ required: true, unique: true })
+  title: string;
+
+  @Prop({ required: true })
+  year: number;
+
+  @Prop({ required: true })
+  director: string;
+
+  @Prop({ required: true })
+  dop: string;
+
+  @Prop({ required: true })
+  cathegory: string;
+
+  img: string;
+}
+
+export const ProjectSchema = SchemaFactory.createForClass(Project);
